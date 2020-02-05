8c82c7f94ea8405e5d1f4b189a70c8442602a966
Daniel Stonier <d.stonier@gmail.com>
2020-02-05 09:55:55 -0500

    Rebase from 'upstream'

--- py_trees_ros/__init__.py
+++ py_trees_ros/__init__.py
@@ -14,13 +14,15 @@ ROS extensions, behaviours and utilities for py_trees.
 # Imports
 ##############################################################################
 
-from . import actions
+from . import action_clients
+from . import actions  # to be deprecated in 2.1.x or later
 from . import battery
 from . import blackboard
 from . import conversions
 from . import exceptions
 from . import mock
 from . import programs
+from . import publishers
 from . import subscribers
 from . import transforms
 from . import trees
